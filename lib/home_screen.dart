54437d11f6afa667aa0179d264aa550ad2053354
ryanemm <emersonryant@gmail.com>
2021-03-07 22:07:43 +0200

    complete appBar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,8 +9,48 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Image.asset("assets/images/logo.png", height: 80),
-        //actions: [],
+        title: Image.asset("assets/images/logo.png", height: 50),
+        backgroundColor: Colors.white,
+        elevation: 40,
+        actions: [
+          Row(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Home",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "About",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Services",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -26,3 +66,70 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+/* TextButton(
+            onPressed: () {},
+            //style: ButtonStyle(backgroundColor:),
+            child: Text(
+              "Home",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 2),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "About",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 2),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            //style: ButtonStyle(backgroundColor:),
+            child: Text(
+              "Services",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 2),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            //style: ButtonStyle(backgroundColor:),
+            child: Text(
+              "Portfolio",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 2),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            //style: ButtonStyle(backgroundColor:),
+            child: Text(
+              "Testimonial",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 2),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            //style: ButtonStyle(backgroundColor:),
+            child: Text(
+              "Contact",
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 2),
+            ),
+          ),
+          */
